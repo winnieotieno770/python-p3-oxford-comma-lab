8c92a8fa147eeee6a1a6a40814a4fcbea5bfdb33
winnieotieno770 <winnieotieno770@gmail.com>
2025-12-30 14:22:53 +0300

    Coverd Array into string using join

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    
+    if len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    
+    if len(items) >= 3:
+        txt = ", ".join(items[:-1]) + f", and {items[-1]}"
+        return txt
+
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))
